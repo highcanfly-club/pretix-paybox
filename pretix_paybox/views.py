28dc170ae3bccd399312c4a30d7185170617ed99
Ronan LE MEILLAT <eltorio@users.noreply.github.com>
2023-08-01 10:19:19 +0200

    handle cancel or failed events

--- pretix_paybox/views.py
+++ pretix_paybox/views.py
@@ -1,31 +1,38 @@
 import sys
 from django.http import HttpResponse
-from django.urls import resolve
-from .payment import check_signed_uuid4, PayboxPayment, getNonce
-from pretix.base.models import Event, Organizer, OrderPayment
-from django_scopes import scope
 from django.shortcuts import redirect, render
-from pretix.multidomain.urlreverse import eventreverse
+from django.urls import resolve
 from django.utils.translation import gettext_lazy as _
+from django_scopes import scope
+from pretix.base.models import Event, OrderPayment, Organizer
+from pretix.multidomain.urlreverse import eventreverse
+from urllib.parse import parse_qs, urlparse
+
 from .PayboxCheck import verify_response
+from .payment import PayboxPayment, check_signed_uuid4, getNonce
 
 
-def refuse(request):
+def refuse(request, *args, **kwargs):
     print('views.refuse', file=sys.stderr)
-    pid = request.GET.get('paymentId')
-    return HttpResponse(_('refused'), status=500)
+    return annule(request, kwargs)
 
 
-def repondre_a(request):
+def repondre_a(request, *args, **kwargs):
     print('views.repondre_a', file=sys.stderr)
-    pid = request.GET.get('paymentId')
-    return HttpResponse(_("answer to"), status=500)
+    return annule(request, kwargs)
+
+
+def get_response_code(request):
+    uri = request.build_absolute_uri()
+    url_parsed = urlparse(uri)
+    query = parse_qs(url_parsed.query)  # dictionnary
+    error = query["error"][0]
+    return error
 
 
 def effectue(request, *args, **kwargs):
     print('views.effectue', file=sys.stderr)
     pid = request.GET.get('paymentId')
-    print(pid, file=sys.stderr)
     if pid == request.session['payment_payboxpayment_uuid4']:
         if request.session.get('paybox_payment_info'):
             paybox_payment_info = request.session.get('paybox_payment_info')
@@ -35,24 +42,41 @@ def effectue(request, *args, **kwargs):
         if payment:
             check = verify_response(request.build_absolute_uri())
             if check:
-                payment.confirm()
-                return redirect(eventreverse(request.event, 'presale:event.order', kwargs={
-                    'order': payment.order.code,
-                    'secret': payment.order.secret
-                }) + '?paid=yes')
+                if get_response_code(request) == "00000":
+                    payment.confirm()
+                    return redirect(eventreverse(request.event, 'presale:event.order', kwargs={
+                        'order': payment.order.code,
+                        'secret': payment.order.secret
+                    }) + '?paid=yes')
+                else:
+                    payment.fail()
+                    return redirect(eventreverse(request.event, 'presale:event.order', kwargs={
+                        'order': payment.order.code,
+                        'secret': payment.order.secret
+                    }))
     return HttpResponse(_("unkown error"), status=200)
 
 
-def annule(request):
+def annule(request, *args, **kwargs):
     print('views.annule', file=sys.stderr)
     pid = request.GET.get('paymentId')
+    if pid == request.session['payment_payboxpayment_uuid4']:
+        check = verify_response(request.build_absolute_uri())
+        if check:
+            if request.session.get('paybox_payment_info'):
+                paybox_payment_info = request.session.get('paybox_payment_info')
+                payment = OrderPayment.objects.get(pk=paybox_payment_info["payment_id"])
+                payment.fail()
+                return redirect(eventreverse(request.event, 'presale:event.order', kwargs={
+                    'order': payment.order.code,
+                    'secret': payment.order.secret
+                }))
     return HttpResponse(_("canceled"), status=500)
 
 
-def attente(request):
+def attente(request, *args, **kwargs):
     print('views.attente', file=sys.stderr)
-    pid = request.GET.get('paymentId')
-    return HttpResponse(_("pending"), status=500)
+    return annule(request, kwargs)
 
 
 def redirectview(request, *args, **kwargs):
